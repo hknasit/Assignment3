6fece2798745da523c791bf700d78d205f889028
Harsh Nasit <harshnasit1100@gmail.com>
2023-11-24 21:59:23 -0500

    first commit
    first commit for the weather app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Assignment3
  Assignment 3 by Harsh Kishor Nasit
+ En. No. C0894125
+
+In this project I have used the weather api for fetching weather information city wise.
+First city name is entered by the user and the a fetch request is made to api which give weathter information in response 
+Then the importend information is displayed on the screen.
--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+import requests
+
+app=Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello world from Flask"
+def homepage():
+    return render_template("index.html")
+
+@app.route("/weather",  methods=['GET', 'POST'])
+def get_weatherdata():
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+
+    # querystring = {"q": "Toronto", "days": "3"}
+    params = {
+        "q" : request.args.get("city")
+    }
+    headers = {
+        "X-RapidAPI-Key": "4ded612ff1mshfa97ab85255debep179561jsn0fae02ef241b",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    print(data)
+    city=data['location']['name']
+    temp=data['current']['temp_c']
+    return f"City: {city}, Tempreture: {temp} "
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
